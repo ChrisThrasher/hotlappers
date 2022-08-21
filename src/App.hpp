d4b77a0c79b40e9552c9ed09410468368073ff0d
Chris Thrasher <chrisjthrasher@gmail.com>
2022-08-20 21:08:10 -0600

    Fix formatting

--- src/App.hpp
+++ src/App.hpp
@@ -3,7 +3,9 @@
 #include <hl/Vehicle.hpp>
 
 class App {
-    sf::RenderWindow m_window { sf::VideoMode({800, 600}), "Hotlappers [indev]", sf::Style::Default ^ sf::Style::Resize };
+    sf::RenderWindow m_window { sf::VideoMode({ 800, 600 }),
+                                "Hotlappers [indev]",
+                                sf::Style::Default ^ sf::Style::Resize };
     Vehicle m_vehicle { sf::Vector2f(m_window.getSize()) / 2.f, sf::Angle::Zero };
     float m_average_fps { 0.f };
     bool m_show_imgui { true };
