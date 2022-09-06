c5918c1e5ffc9fbe95902dd2b848f77324f09d19
Chris Thrasher <chrisjthrasher@gmail.com>
2022-09-05 18:17:36 -0600

    Use angle literals

--- tests/Vehicle.cpp
+++ tests/Vehicle.cpp
@@ -3,6 +3,7 @@
 #include <catch2/catch_all.hpp>
 
 using Approx = Catch::Approx;
+using namespace sf::Literals;
 
 namespace sf {
 std::ostream& operator<<(std::ostream& out, const Vector2f& v) { return out << '(' << v.x << ", " << v.y << ')'; }
@@ -14,11 +15,11 @@ TEST_CASE("Vehicle class")
 {
     SECTION("Construction")
     {
-        const Vehicle vehicle({ 1, 2 }, sf::degrees(3));
+        const Vehicle vehicle({ 1, 2 }, 3_deg);
         CHECK(vehicle.getPosition() == sf::Vector2f(1, 2));
         CHECK(vehicle.getVelocity() == sf::Vector2f());
-        CHECK(vehicle.getRotation() == sf::degrees(3));
-        CHECK(vehicle.getYawRate() == sf::degrees(0));
+        CHECK(vehicle.getRotation() == 3_deg);
+        CHECK(vehicle.getYawRate() == 0_deg);
     }
 
     SECTION("Drive forward then brake")
@@ -29,15 +30,15 @@ TEST_CASE("Vehicle class")
         CHECK(vehicle.getPosition().y == 0.f);
         CHECK(vehicle.getVelocity().x > 0.f);
         CHECK(vehicle.getVelocity().y == 0.f);
-        CHECK(vehicle.getRotation() == sf::degrees(0));
-        CHECK(vehicle.getYawRate() == sf::degrees(0));
+        CHECK(vehicle.getRotation() == 0_deg);
+        CHECK(vehicle.getYawRate() == 0_deg);
 
         vehicle.update(sf::seconds(1), {}, 1, {});
         CHECK(vehicle.getPosition().x > 0.f);
         CHECK(vehicle.getPosition().y == 0.f);
         CHECK(vehicle.getVelocity().x == Approx(0).margin(0.1));
         CHECK(vehicle.getVelocity().y == 0.f);
-        CHECK(vehicle.getRotation() == sf::degrees(0));
-        CHECK(vehicle.getYawRate() == sf::degrees(0));
+        CHECK(vehicle.getRotation() == 0_deg);
+        CHECK(vehicle.getYawRate() == 0_deg);
     }
 }
